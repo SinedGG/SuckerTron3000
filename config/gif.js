229d3f6b2ca502f0fdb7c99c7623f8ed4033612a
den <51417134+SinedGG@users.noreply.github.com>
2025-01-11 12:24:28 +0200

    Update gif.js

--- config/gif.js
+++ config/gif.js
@@ -1,6 +1,7 @@
 module.exports = {
-    fire: "https://cdn.discordapp.com/attachments/1327231582305783858/1327250409848307773/fire_1f5251-ezgif.com-apng-to-gif-converter.gif?ex=678261c4&is=67811044&hm=5b1cf3d8f0377eeaf8ee7b408b1c61b37b0c711e40f45166f8b2504d8f6cdb61&",
-    star: "https://fonts.gstatic.com/s/e/notoemoji/latest/1f31f/512.gif",
-    hands: "https://fonts.gstatic.com/s/e/notoemoji/latest/1f64c/512.gif",
-    lottie: "https://fonts.gstatic.com/s/e/notoemoji/latest/2728/512.gif"
-}
+  fire: "https://cdn.discordapp.com/attachments/1327231582305783858/1327250409848307773/fire_1f5251-ezgif.com-apng-to-gif-converter.gif?ex=678261c4&is=67811044&hm=5b1cf3d8f0377eeaf8ee7b408b1c61b37b0c711e40f45166f8b2504d8f6cdb61&",
+  star: "https://fonts.gstatic.com/s/e/notoemoji/latest/1f31f/512.gif",
+  hands: "https://fonts.gstatic.com/s/e/notoemoji/latest/1f64c/512.gif",
+  lottie: "https://fonts.gstatic.com/s/e/notoemoji/latest/2728/512.gif",
+  fireworks: "https://fonts.gstatic.com/s/e/notoemoji/latest/1f386/512.gif",
+};
