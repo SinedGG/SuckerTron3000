70b1808b0a1114c0476a3a7e06a7a3b7c0199758
den <51417134+SinedGG@users.noreply.github.com>
2025-01-11 12:36:26 +0200

    Update give_role.js

--- modules/xp/give_role.js
+++ modules/xp/give_role.js
@@ -33,7 +33,7 @@ module.exports = async (client) => {
         "<a:starsded:1327385723975893094> Система досвіду - єБали <a:starsded:1327385723975893094>"
       )
       .setDescription(
-        "<a:partypoppersded:1327417596634464256>Вітаю <@274449343124078593> , ви покращили свій рівень і здобули роль <@&1326269871159378072>"
+        `<a:partypoppersded:1327417596634464256>Вітаю <@${member.user.id}> , ви покращили свій рівень і здобули роль <@&${role.id}>`
       )
       .setFooter({ text: "SDED Community", iconURL: gif.lottie })
       .setColor("#FFA500");
