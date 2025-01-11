864f110ed73579833ddcbc32f0b2265ffe524950
den <51417134+SinedGG@users.noreply.github.com>
2025-01-11 15:46:27 +0200

    Update voice.js

--- modules/xp/voice.js
+++ modules/xp/voice.js
@@ -1,6 +1,5 @@
 module.exports = (client) => {
   const xp = require("@models/xp");
-  const cfg = require("@config/xp");
 
   const { ChannelType } = require("discord.js");
 
@@ -10,7 +9,7 @@ module.exports = (client) => {
     (channel) => channel.type === ChannelType.GuildVoice
   );
 
-  voiceChannels.forEach((voiceChannel) => {
+  voiceChannels.forEach(async (voiceChannel) => {
     if (
       voiceChannel.members.size === 0 ||
       voiceChannel.id === guild.afkChannelId
@@ -25,25 +24,49 @@ module.exports = (client) => {
         !member.user.bot &&
         !guild.voiceStates.cache.get(member.id).selfDeaf
       ) {
-        activeUsers.push(member);
+        const user_activity = member.presence.activities
+          .filter((activity) => activity.name !== "Custom Status")
+          .map((activity) => activity.name);
+        activeUsers.push({
+          id: member.id,
+          name: member.user.username,
+          mute: guild.voiceStates.cache.get(member.id).selfMute,
+          activity: user_activity,
+        });
+      }
+    });
+    const result = calculateScores(activeUsers);
+    for (let i = 0; i < result.length; i++) {
+      const user = result[i];
+      const user_score = user.score;
+      await xp.add(user.id, user.name, user_score);
+      console.log(
+        `[XP] ${user.name} got ${user_score} points for being active in voice channel`
+      );
+    }
+  });
+};
+
+const calculateScores = (data) => {
+  const cfg = require("@config/xp");
+  return data.map((user, _, array) => {
+    let score = user.mute ? cfg.points.voiceMuted : cfg.points.voice;
+
+    array.forEach((otherUser) => {
+      if (user.id !== otherUser.id) {
+        const hasCommonActivity = user.activity.some((activity) =>
+          otherUser.activity.includes(activity)
+        );
+        if (
+          hasCommonActivity &&
+          user.activity.length > 0 &&
+          otherUser.activity.length > 0
+        ) {
+          score += 2;
+        }
       }
     });
 
-    if (activeUsers.length > 1) {
-      activeUsers.forEach(async (member) => {
-        const voiceState = guild.voiceStates.cache.get(member.id);
-        if (voiceState.selfMute) {
-          await xp.add(member.id, member.user.tag, cfg.points.voiceMuted);
-          console.log(
-            `[XP] ${member.user.tag} is muted + ${cfg.points.voiceMuted} xp`
-          );
-        } else {
-          await xp.add(member.id, member.user.tag, cfg.points.voice);
-          console.log(
-            `[XP] ${member.user.tag} is not muted + ${cfg.points.voice} xp`
-          );
-        }
-      });
-    }
+    return { ...user, score };
   });
 };
