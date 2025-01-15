090ef7c118d6223871772ef1ec6adfb4f5c49990
SinedGG <denis2320032@gmail.com>
2025-01-15 10:28:53 +0200

    Fixed streak reset

--- modules/streak/renew.js
+++ modules/streak/renew.js
@@ -7,7 +7,9 @@ module.exports = async (client) => {
     const user_score = streak_data[i].User.Xp.score;
     await streak.update_last_score(streak_data[i].user_id, user_score);
     if (streak_data[i].give_today == false) {
+      await streak.update_streak(streak_data[i].user_id, 0);
       update_name(client, streak_data[i].User.ds_id, ``);
+      console.log(`[Streak] ${streak_data[i].User.ds_name} streak reset`);
     } else {
       await streak.update_give_today(streak_data[i].user_id, false);
     }
